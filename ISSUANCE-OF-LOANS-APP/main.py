aba36cbbdf066c0aff3839028f36f54701575e21
dorofeichik <dorofeichik9723@gmail.com>
2021-02-18 11:21:24 +0300

    added deletion of editing loan applications

--- ISSUANCE-OF-LOANS-APP/main.py
+++ ISSUANCE-OF-LOANS-APP/main.py
@@ -18,12 +18,13 @@ class loan_issuance_list(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     loan_name = db.Column(db.String(50), nullable=False)
     client_passport_number = db.Column(db.String(50), nullable=False)
-    #add_date = db.Column(db.DateTime, default=datetime.utcnow)
+    # add_date = db.Column(db.DateTime, default=datetime.utcnow)
     loan_amount = db.Column(db.Integer, nullable=False)
     loan_term = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return '<loan_issuance_list %r>' %self.id
+        return "<loan_issuance_list %r>" % self.id
+
 
 class clients_list(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -41,7 +42,8 @@ class clients_list(db.Model):
         self.number_of_loan_applications = number_of_loan_applications
 
     def __repr__(self):
-        return '<clients_list %r>' %self.id
+        return "<clients_list %r>" % self.id
+
 
 class loans_list(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -55,7 +57,7 @@ class loans_list(db.Model):
         self.number_of_loan_applications = number_of_loan_applications
 
     def __repr__(self):
-        return '<loans_list %r>' %self.id
+        return "<loans_list %r>" % self.id
 
 
 @app.route("/")
@@ -65,37 +67,70 @@ def index():
 
 @app.route("/loan_issuance_list")
 def Loan_issuance_list():
-    Loan_issuance_list_table = loan_issuance_list.query.all()
-    return render_template("loan_issuance_list.html", Loan_issuance_list_table=Loan_issuance_list_table)
+    Loan_issuance_list_table = loan_issuance_list.query.order_by(
+        loan_issuance_list.id.desc()
+    ).all()
+    return render_template(
+        "loan_issuance_list.html", Loan_issuance_list_table=Loan_issuance_list_table
+    )
 
 
-@app.route("/add_loan_issuance", methods=['POST','GET'])
+@app.route("/add_loan_issuance", methods=["POST", "GET"])
 def add_loan_issuance():
     if request.method == "POST":
-        loan_name = request.form['loan_name']
-        client_passport_number = request.form['client_passport_number']
-        loan_amount = request.form['loan_amount']
-        loan_term = request.form['loan_term']
+        loan_name = request.form["loan_name"]
+        client_passport_number = request.form["client_passport_number"]
+        loan_amount = request.form["loan_amount"]
+        loan_term = request.form["loan_term"]
 
-        Loan_issuance_list = loan_issuance_list(loan_name=loan_name,
+        Loan_issuance_list = loan_issuance_list(
+            loan_name=loan_name,
             client_passport_number=client_passport_number,
             loan_amount=loan_amount,
-            loan_term=loan_term
+            loan_term=loan_term,
         )
 
         try:
             db.session.add(Loan_issuance_list)
             db.session.commit()
-            return redirect('/loan_issuance_list')
+            return redirect("/loan_issuance_list")
         except:
             return "an error occurred while adding an article"
     else:
         return render_template("add_loan_issuance.html")
 
 
-@app.route("/edit_loan_issuance")
-def edit_loan_issuance():
-    return render_template("edit_loan_issuance.html")
+@app.route("/loan_issuance_list/<int:id>/edit", methods=["POST", "GET"])
+def edit_loan_issuance(id):
+    edit_element = loan_issuance_list.query.get(id)
+    if request.method == "POST":
+        edit_element.loan_name = request.form["loan_name"]
+        edit_element.client_passport_number = request.form["client_passport_number"]
+        edit_element.loan_amount = request.form["loan_amount"]
+        edit_element.loan_term = request.form["loan_term"]
+
+        try:
+            db.session.commit()
+            return redirect("/loan_issuance_list")
+        except:
+            return "error while editing"
+    else:
+
+        return render_template(
+            "edit_loan_issuance.html", edit_element=edit_element
+        )
+
+
+@app.route("/loan_issuance_list/<int:id>/del")
+def delete_loan_issuance(id):
+    Loan_issuance_list_delete_element = loan_issuance_list.query.get_or_404(id)
+
+    try:
+        db.session.delete(Loan_issuance_list_delete_element)
+        db.session.commit()
+        return redirect("/loan_issuance_list")
+    except:
+        return "an error occurred while deleting the article"
 
 
 if __name__ == "__main__":
